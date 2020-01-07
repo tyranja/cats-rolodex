b0e6cb671dd5da8203907513f6207d2f2bcc1625
Tyranja <tyranja@cassiopeia.uberspace.de>
2020-01-07 14:59:03 +0100

    Updates

---
+++ precache-manifest.39cc37d50b2873561e0fce81e2eb95ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40092d476d37a1ebc43f488658cfa2ba",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "a555f25cced28d0c3082",
+    "url": "/cats-rolodex/static/css/main.4879d5f8.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/cats-rolodex/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cats-rolodex/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a555f25cced28d0c3082",
+    "url": "/cats-rolodex/static/js/main.8a7e53dc.chunk.js"
+  },
+  {
+    "revision": "85eec7738b32c02bca47",
+    "url": "/cats-rolodex/static/js/runtime-main.b0abadb6.js"
+  }
+]);
